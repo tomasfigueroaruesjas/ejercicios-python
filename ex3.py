9ab37110947b2afc43e5eb49afe12d5a1e5d0d16
tomas figueroa <tomasfigueroaruesjas@gmail.com>
2022-06-01 16:29:42 -0300

    Primera commit. Ejercicios python

---
+++ ex3.py
@@ -0,0 +1,3 @@
+pisoEu = int(input('Piso europeo? '))
+pisoUs = pisoEu + 1
+print('Equivalente americano: ', pisoUs)
---
+++ ex4.py
@@ -0,0 +1,4 @@
+horas = int(input('Ingrese cant horas: '))
+rate = float(input('Ingrese rate: '))
+paga = horas * rate
+print('La paga total es: ', paga)
